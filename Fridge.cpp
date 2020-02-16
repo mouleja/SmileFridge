6cf80df16cb00bc82baba77d210c790b28bf27c7
JasonM <mouelja@oregonstate.edu>
2020-02-16 14:09:48 -0800

    Added Quantity ordered on info display

--- Fridge.cpp
+++ Fridge.cpp
@@ -118,3 +118,77 @@ void Fridge::AddItem(string sku, int quantity) {
 		this->_contents[place]->quantity += quantity;
 	}
 }
+
+
+void Fridge::orderLowItems() {
+
+	for (std::map<string, ItemInfo*>::iterator it = _items.begin(); it != _items.end(); it++) {
+
+		//only add the item to the order list if it has a designated minimum quantity threshold
+		if (it->second->minQuantity > 0) {
+
+			int index = GetIndexBySku(it->first);
+
+			//if the fridge is not currently stocked with the item
+			if (index == -1) {
+				int orderMultiple = 1;
+				//ensure amount to be ordered is greater than the designated minimum quantity for the item
+				while (orderMultiple * it->second->orderQuantity < it->second->minQuantity) orderMultiple++;
+				//add item to order list
+				orderList.insert({ it->first, orderMultiple });
+				/* No need to worry about adding too many items to the orderList if orderLowItems() is called multiple times
+				   before an order is received since the orderList is a map and the appropriate quantity will be overwritten
+				   at the corresponding sku key rather than indexed multiple times as a {key, value} element in the map */
+			}//end if
+
+			//if the fridge has a current quantity for the item and it's below minQuantity
+			else if (_contents[index]->quantity < it->second->minQuantity) {
+				int orderMultiple = 1;
+				int currentQuantity = _contents[index]->quantity + it->second->orderQuantity;
+				while (currentQuantity < it->second->minQuantity) {
+					currentQuantity += it->second->orderQuantity;
+					orderMultiple++;
+				}//end while
+				//add item and quantity to order list
+				orderList.insert({ it->first, orderMultiple });
+			}//end else if
+
+		}//end outer if
+
+	}//end for loop
+}//end orderLowItems
+
+void Fridge::updateInventory() {
+	std::ofstream outFile(INVFILE, std::ios::trunc);
+	for (FridgeItem* fridgeItem : _contents) {
+		ItemInfo* item = fridgeItem->itemInfo;
+		outFile << item->sku << "," << fridgeItem->quantity << "," << fridgeItem->dateStocked.Year << ","
+		<< fridgeItem->dateStocked.Days	<< "," << fridgeItem->goodFor << std::endl;
+	}
+	outFile.close();
+}
+
+void Fridge::placeOrder() {
+	for (std::map<string, int>::iterator it = orderList.begin(); it != orderList.end(); it++) {
+		int index = GetIndexBySku(it->first);
+		if (index == -1) {
+			/*Hardcoded fridge data temporarily: in actual application get it from Date Time function when order is received*/
+			FridgeItem* newItem = new FridgeItem(_items[it->first], it->second, GetCurrentDate(), 90);
+			_contents.push_back(newItem);
+		}
+		else {
+			FridgeItem* updateItem = _contents[GetIndexBySku(it->first)];
+			updateItem->quantity += (it->second * updateItem->itemInfo->orderQuantity);
+		}
+	}
+	orderList.clear();
+}
+
+void Fridge::printOrderList() {
+	std::cout << "Item name | Order Units | Quantity Ordered" << std::endl << std::endl;
+	for (std::map<string, int>::iterator it = orderList.begin(); it != orderList.end(); it++) {
+		ItemInfo* orderItem = _items.at(it->first);
+		std::cout << orderItem->displayName << " | " << it->second 
+			<< " | " << (orderItem->orderQuantity * it->second) << std::endl;
+	}
+}
