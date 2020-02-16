da1de5b4090b987993e9a7cd7d49277a1f8d0e7a
Tommy Armstrong <armstrth@oregonstate.edu>
2020-02-15 23:01:20 -0800

    Updated the following within Fridge.cpp: orderLowItems(), placeOrder() -- To accommodate the issue of an item with a minimum threshold > 0 and Fridge quantity = 0.

--- Fridge.cpp
+++ Fridge.cpp
@@ -75,19 +75,42 @@ int Fridge::GetIndexBySku(string sku)
 }
 
 void Fridge::orderLowItems() {
-	for (int i = 0; i < _contents.size(); i++) {
-		if (_contents[i]->quantity < _contents[i]->itemInfo->minQuantity) {
-			int orderMultiple = 1;
-			int currentQuant = _contents[i]->quantity;
-			int minQuant = _contents[i]->itemInfo->minQuantity;
-			while (currentQuant < minQuant) {
-				currentQuant += _contents[i]->itemInfo->orderQuantity;
-				orderMultiple++;
-			}//end while
-			orderList.insert({ _contents[i]->itemInfo->sku, orderMultiple });
-		}//end if
-	}//end for
-}
+
+	for (std::map<string, ItemInfo*>::iterator it = _items.begin(); it != _items.end(); it++) {
+		
+		//only add the item to the order list if it has a designated minimum quantity threshold
+		if (it->second->minQuantity > 0) {
+
+			int index = GetIndexBySku(it->first);
+
+			//if the fridge is not currently stocked with the item
+			if (index == -1) {
+				int orderMultiple = 1;
+				//ensure amount to be ordered is greater than the designated minimum quantity for the item
+				while (orderMultiple * it->second->orderQuantity < it->second->minQuantity) orderMultiple++;
+				//add item to order list
+				orderList.insert({ it->first, orderMultiple });
+				/* No need to worry about adding too many items to the orderList if orderLowItems() is called multiple times
+				   before an order is received since the orderList is a map and the appropriate quantity will be overwritten
+				   at the corresponding sku key rather than indexed multiple times as a {key, value} element in the map */
+			}//end if
+
+			//if the fridge has a current quantity for the item and it's below minQuantity
+			else if (_contents[index]->quantity < it->second->minQuantity) {
+				int orderMultiple = 1;
+				int currentQuantity = _contents[index]->quantity + it->second->orderQuantity;
+				while (currentQuantity < it->second->minQuantity) {
+					currentQuantity += it->second->orderQuantity;
+					orderMultiple++;
+				}//end while
+				//add item and quantity to order list
+				orderList.insert({ it->first, orderMultiple });
+			}//end else if
+
+		}//end outer if
+
+	}//end for loop
+}//end orderLowItems
 
 void Fridge::updateInventory() {
 	std::ofstream outFile(INVFILE, std::ios::trunc);
@@ -101,8 +124,16 @@ void Fridge::updateInventory() {
 
 void Fridge::placeOrder() {
 	for (std::map<string, int>::iterator it = orderList.begin(); it != orderList.end(); it++) {
-		FridgeItem* updateItem = _contents[GetIndexBySku(it->first)];
-		updateItem->quantity += (it->second * updateItem->itemInfo->orderQuantity);
+		int index = GetIndexBySku(it->first);
+		if (index == -1) {
+			/*Hardcoded fridge data temporarily: in actual application get it from Date Time function when order is received*/
+			FridgeItem *newItem = new FridgeItem(_items.at(it->first), (it->second * _items.at(it->first)->orderQuantity), 2020, 46, 30);
+			_contents.push_back(newItem);
+		}
+		else {
+			FridgeItem* updateItem = _contents[GetIndexBySku(it->first)];
+			updateItem->quantity += (it->second * updateItem->itemInfo->orderQuantity);
+		}
 	}
 	orderList.clear();
 }
